687164819163ae6191c7f3c775820e4e2dd61da7
MassinissaDjellouli <79031818+MassinissaDjellouli@users.noreply.github.com>
2022-05-02 13:11:32 -0400

    Creation du composant employes

--- tp4react/src/App.js
+++ tp4react/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import { Users } from './Components/users';
+import { Employes } from './Components/PageServices/employes';
+import { Users } from './Components/PageServices/users';
 function App() {
   return (
     <>
       <h1 class="bg-dark text-light text-center py-4">Bibliothèque de JavaTown</h1>
       <Users />
+      <Employes />
     </>
   );
 }
---
+++ tp4react/src/Components/PageServices/employes.js
@@ -0,0 +1,8 @@
+import React from 'react'
+export const Employes = () => {
+    return (
+        <>
+            <h1 class="bg-primary text-light py-3 text-center">Portail employé</h1>
+        </>
+    )
+}
