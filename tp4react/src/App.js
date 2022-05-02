8863cb97cb954463c60719a0ab7d6e9f1b88089d
MassinissaDjellouli <79031818+MassinissaDjellouli@users.noreply.github.com>
2022-05-02 13:19:34 -0400

    Ajout du routage

--- tp4react/src/App.js
+++ tp4react/src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import { Employes } from './Components/PageServices/employes';
 import { Users } from './Components/PageServices/users';
+import { Link, Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <>
       <h1 class="bg-dark text-light text-center py-4">Bibliothèque de JavaTown</h1>
-      <Users />
-      <Employes />
+    <Routes>
+      <Route path="users" element={<Users />}></Route>
+      <Route path="employes" element={<Employes />}></Route>
+    </Routes>
+      <Link to="/users">Portail utilisateur</Link>
+      <Link to="/employes">Portail employé</Link>
+
     </>
   );
 }
