7a7abf69ac9818bc3d7bfd65070da0629aa70b36
MassinissaDjellouli <79031818+MassinissaDjellouli@users.noreply.github.com>
2022-05-04 17:16:09 -0400

    Utilisation du header et de la liste d'employés

--- tp4react/src/Components/PageServices/employes.js
+++ tp4react/src/Components/PageServices/employes.js
@@ -1,20 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import { Routes, Route } from 'react-router-dom';
-import { Home } from '../home'
-import { EmployeHome } from './EmployeeService/employeeHome';
-export const Employes = () => {
+import { EmployeHeader } from '../Headers/employeHeader';
+import { EmployeeList } from './EmployeeService/employeList';
+export const Employes = ({employes}) => {
     return (
         <>
-            <h1 className="bg-primary text-light py-3 text-center">Portail employé</h1>
+            <EmployeHeader />
+            <EmployeeList employees={employes}/>
             <div className="row d-flex justify-content-center align-items-center">
-                    <Link to="/" className="col-2 text-light text-decoration-none">
+                    <Link to="/23" className="col-2 text-light text-decoration-none">
                         <button className="col-12 btn btn-primary m-3 py-2">Page d'accueil</button>
                     </Link>
             </div>
-            <Routes>
-              <Route path="" element={<EmployeHome />}></Route>
-            </Routes>
         </>
     )
 }
