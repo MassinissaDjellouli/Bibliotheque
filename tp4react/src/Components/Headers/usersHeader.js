c6d8b592ce4223008775150fde21c6875f476288
MassinissaDjellouli <79031818+MassinissaDjellouli@users.noreply.github.com>
2022-05-04 23:17:18 -0400

    Ajout du composant user header

---
+++ tp4react/src/Components/Headers/usersHeader.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Link } from "react-router-dom";
+export const UserHeader = () => {
+    return(
+    <Link className='text-decoration-none' to="/users">
+        <h1 class="bg-secondary text-light py-3 text-center">Portail utilisateur</h1>
+    </Link>
+    )
+}
--- tp4react/src/Components/PageServices/users.js
+++ tp4react/src/Components/PageServices/users.js
@@ -2,11 +2,12 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import { UserHome } from './UserService/userHome';
 import { Routes, Route } from 'react-router-dom';
+import { UserHeader } from '../Headers/usersHeader';
 
 export const Users = () => {
     return (
         <>
-            <h1 class="bg-secondary text-light py-3 text-center">Portail utilisateur</h1>
+        <UserHeader />
             <div className="row d-flex justify-content-center align-items-center">
                     <Link to="/" className="col-2 text-light text-decoration-none">
                         <button className="col-12 btn btn-secondary m-3 py-2">Page d'accueil</button>
