d51b1a33d4825a5b58e2d58e0bba8d3370795ba2
MassinissaDjellouli <79031818+MassinissaDjellouli@users.noreply.github.com>
2022-05-05 15:20:57 -0400

    modification du composant users pour ajouter l'option de s'inscrire

--- tp4react/src/Components/PageServices/users.js
+++ tp4react/src/Components/PageServices/users.js
@@ -4,18 +4,27 @@ import { UserHome } from './UserService/userHome';
 import { Routes, Route } from 'react-router-dom';
 import { UserHeader } from '../Headers/usersHeader';
 import { UserList } from './UserService/userList';
+import { NewUser } from './UserService/newUser';
 
 export const Users = ({users}) => {
 
     return (
+        
         <>
+        
         <UserHeader />
+        <div className='row d-flex justify-content-center '>
+                <h1 className='col-8 text-center m-4'>Inscription</h1>
+                <Link to="/newUser" className='col-6 justify-content-center d-flex text-decoration-none'>
+                    <button className='col-12 btn btn-outline-secondary p-4'>Nouveau client</button>
+                </Link>
+        </div>
         <UserList users={users} />
-            <div className="row d-flex justify-content-center align-items-center">
-                    <Link to="/" className="col-2 text-light text-decoration-none">
-                        <button className="col-12 btn btn-secondary m-3 py-2">Page d'accueil</button>
-                    </Link>
-            </div>
+        <div className="row d-flex justify-content-center">
+                <Link to="/" className="col-2 text-light text-decoration-none">
+                    <button className="col-12 btn btn-secondary m-3 py-2">Page d'accueil</button>
+                </Link>
+        </div>
 
         </>
     )
