ae3236c9dfc6a98df8ceb35689cabf13668197f3
MassinissaDjellouli <79031818+MassinissaDjellouli@users.noreply.github.com>
2022-05-07 15:30:15 -0400

    recuperation des données du formulaire

--- tp4react/src/Components/PageServices/UserService/userCreationForm.js
+++ tp4react/src/Components/PageServices/UserService/userCreationForm.js
@@ -1,21 +1,39 @@
 import React from 'react'
 import { UserHeader } from '../../Headers/usersHeader'
 import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react'
+
 export const UserCreationForm = () => {
+    const [clientName,setName] = useState('');
+    const [phone,setPhone] = useState('');
+    const [adress,setAdress] = useState('');
+    const submit = (e) => {
+        e.preventDefault()
+        console.log(clientName)
+        console.log(phone)
+        console.log(adress)
+    }
     return (
-           <div>
+        <form onSubmit={submit} >
             <div>
-                <label for="name">Nom et prénom</label>
-                <input type="text" placeholder="2-45 characters" name="name" id="name" />
+                <label className='form-label' >Nom et prénom</label>
+                <input className='form-control' type="text" placeholder="2-45 characters"  
+                onChange={(e) => {setName(e.target.value)}}/>
             </div>
-            <div class="formbuilder-text form-group field-phone">
-                <label for="phone">Numéro de téléphone</label>
-                <input type="text" name="phone" />
+            <div>
+                <label className='form-label' >Numéro de téléphone</label>
+                <input className='form-control' type="text" 
+                onChange={(e) => {setPhone(e.target.value)}} />
             </div>
-            <div class="formbuilder-text form-group field-adresse">
-                <label for="adresse" >Adresse</label>
-                <input type="text" name="adresse" />
+            <div>
+                <label className='form-label'>Adresse</label>
+                <input className='form-control' type="text"
+                onChange={(e) => {setAdress(e.target.value)}} />
             </div>
-        </div>
+            <div className='row d-flex justify-content-center'>
+                <button className='btn btn-secondary mt-4 col-4'>Envoyer</button>
+            </div>
+        </form>
     )
 }
+
