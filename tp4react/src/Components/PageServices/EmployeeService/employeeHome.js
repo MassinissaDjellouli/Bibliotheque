d76210e248b732405ccc2943898caaf2f6b1a84d
MassinissaDjellouli <79031818+MassinissaDjellouli@users.noreply.github.com>
2022-05-07 19:59:45 -0400

    Ajout d'un accordéon dans le menu employé

--- tp4react/src/Components/PageServices/EmployeeService/employeeHome.js
+++ tp4react/src/Components/PageServices/EmployeeService/employeeHome.js
@@ -17,6 +17,37 @@ export const EmployeHome = ({employes}) => {
             <EmployeHeader />
             <div className='container'>
                 <h3>Bienvenue {employee.username}</h3>
+                <div class="accordion" id="accordeon">
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingOne">
+                            <button class="accordion-button collapsed" type="button" 
+                            data-bs-toggle="collapse" data-bs-target="#collapseEmprunt">
+                                Ajouter un livre
+                            </button>
+                        </h2>
+                        <div id="collapseEmprunt" class="accordion-collapse collapse"
+                        data-bs-parent="#accordeon">
+                            <div class="accordion-body">
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingTwo">
+                            <button class="accordion-button collapsed" type="button"
+                            data-bs-toggle="collapse" data-bs-target="#collapseRetour">
+                                Ajouter un média
+
+                            </button>
+                        </h2>
+                        <div id="collapseRetour" class="accordion-collapse collapse" 
+                        data-bs-parent="#accordeon">
+                            <div class="accordion-body">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     )
