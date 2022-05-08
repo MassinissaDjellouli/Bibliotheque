3082a0df8d99d69f00c8b19b740dc050888843c9
MassinissaDjellouli <79031818+MassinissaDjellouli@users.noreply.github.com>
2022-05-08 18:43:51 -0400

    Ajout du composant de cartes d'emprunt

---
+++ tp4react/src/Components/PageServices/empruntCard.js
@@ -0,0 +1,15 @@
+import React from "react";
+export const EmpruntCard = ({ emprunt }) => {
+    return (
+        <div className="card m-3">
+            <div className="card-header">
+                <h4 className="card-title">{emprunt.documentName}</h4>
+            </div>
+            <div className="card-body">
+                <h6>Date d'emprunt: {emprunt.empruntDate}</h6>
+                <h6>Date de retour: {emprunt.returnDate}</h6>
+                <h6>Retourné: {emprunt.returned == "true" ? "oui":"non"}</h6>
+            </div>
+        </div>
+    )
+}
