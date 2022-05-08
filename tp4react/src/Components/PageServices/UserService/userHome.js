6ed7eaa5a2fb78b659abec6a2b3aa7a58b85cc5b
MassinissaDjellouli <79031818+MassinissaDjellouli@users.noreply.github.com>
2022-05-08 18:44:17 -0400

    Ajout de la liste d'emprunts dans le home de l'utilisateur

--- tp4react/src/Components/PageServices/UserService/userHome.js
+++ tp4react/src/Components/PageServices/UserService/userHome.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { UserHeader } from '../../Headers/usersHeader'
 import { Link, Route, Routes, useParams } from 'react-router-dom';
+import { EmpruntList } from './listeEmprunts';
 
 
 export const UserHome = ({ getUser }) => {
+    const [emprunts,setEmprunts] = useState([]);
+    useEffect(() => {
+        getEmprunts(user);
+    })
+    const getRequest = async (path) => {
+        let request = await fetch("http://localhost:8080" + path)
+        let data = await request.json();
+        return data;
+      }
+
+    const getEmprunts = async (user) => {
+        let data = await getRequest("/users/" + user.clientNumber + "/emprunts");
+        setEmprunts(data);
+      }
+
     let clientNumber = useParams().id;
     let user = getUser(clientNumber);
     if (user == undefined) {
@@ -14,17 +30,17 @@ export const UserHome = ({ getUser }) => {
             <UserHeader />
             <div className='container'>
                 <h3 className='my-4'>Bienvenue {user.clientName}</h3>
-                <div class="accordion" id="accordeon">
-                    <div class="accordion-item">
-                        <h2 class="accordion-header" id="headingOne">
-                            <button class="accordion-button collapsed" type="button"
+                <div className="accordion" id="accordeon">
+                    <div className="accordion-item">
+                        <h2 className="accordion-header" id="headingOne">
+                            <button className="accordion-button collapsed" type="button"
                                 data-bs-toggle="collapse" data-bs-target="#collapseEmprunt">
                                 Emprunter un livre
                             </button>
                         </h2>
-                        <div id="collapseEmprunt" class="accordion-collapse collapse"
+                        <div id="collapseEmprunt" className="accordion-collapse collapse"
                             data-bs-parent="#accordeon">
-                            <div class="accordion-body d-flex justify-content-center">
+                            <div className="accordion-body d-flex justify-content-center">
                                 <Link to="emprunter">
                                     <button className='btn btn-success'>Emprunter</button>
                                 </Link>
@@ -32,32 +48,32 @@ export const UserHome = ({ getUser }) => {
                             </div>
                         </div>
                     </div>
-                    <div class="accordion-item">
-                        <h2 class="accordion-header" id="headingTwo">
-                            <button class="accordion-button collapsed" type="button"
+                    <div className="accordion-item">
+                        <h2 className="accordion-header" id="headingTwo">
+                            <button className="accordion-button collapsed" type="button"
                                 data-bs-toggle="collapse" data-bs-target="#collapseRetour">
                                 Retourner un livre
 
                             </button>
                         </h2>
-                        <div id="collapseRetour" class="accordion-collapse collapse"
+                        <div id="collapseRetour" className="accordion-collapse collapse"
                             data-bs-parent="#accordeon">
-                            <div class="accordion-body">
+                            <div className="accordion-body">
 
                             </div>
                         </div>
                     </div>
-                    <div class="accordion-item">
-                        <h2 class="accordion-header" id="headingThree">
-                            <button class="accordion-button collapsed" type="button"
+                    <div className="accordion-item">
+                        <h2 className="accordion-header" id="headingThree">
+                            <button className="accordion-button collapsed" type="button"
                                 data-bs-toggle="collapse" data-bs-target="#collapseListe">
                                 Voir ma liste d'emprunts
                             </button>
                         </h2>
-                        <div id="collapseListe" class="accordion-collapse collapse"
+                        <div id="collapseListe" className="accordion-collapse collapse"
                             data-bs-parent="#accordeon">
-                            <div class="accordion-body">
-
+                            <div className="accordion-body">
+                                <EmpruntList emprunts={emprunts}/>
                             </div>
                         </div>
                     </div>
