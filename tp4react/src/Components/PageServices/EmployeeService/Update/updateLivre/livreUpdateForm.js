fbdc9a7abcfef167da5d85977721f5f4cf5ddab4
MassinissaDjellouli <79031818+MassinissaDjellouli@users.noreply.github.com>
2022-05-10 14:49:59 -0400

    Ajout de composants pour les formulaires de modification des documents

---
+++ tp4react/src/Components/PageServices/EmployeeService/Update/updateLivre/livreUpdateForm.js
@@ -0,0 +1,90 @@
+import React from 'react'
+import { useState } from 'react'
+import { EmployeHeader } from '../../../../Headers/employeHeader';
+import { DocumentCard } from '../../../UserService/Documents/documentCard';
+
+export const LivreUpdateForm = ({ submit, livre }) => {
+
+    const [titre, setTitre] = useState('');
+    const [auteur, setAuteur] = useState('');
+    const [editeur, setEditeur] = useState('');
+    const [anneeDePublication, setDate] = useState("2000");
+    const [nbExemplaires, setNbEx] = useState("1");
+    const [nbPages, setNbPages] = useState("1");
+    const [genre, setGenre] = useState("roman");
+
+    const onSubmit = (e) => {
+        let tempsEmprunt = 3;
+        const updatedLivre = { titre, auteur, editeur, anneeDePublication, tempsEmprunt, nbExemplaires, nbPages, genre }
+        e.preventDefault()
+        submit(updatedLivre,livre.documentId)
+    }
+    if(livre.titre == undefined){
+        return <>
+            <EmployeHeader />
+            <h1 className='text-center text-danger'>Pas de livre</h1>
+        </>
+    }
+    return (
+        <>
+            <EmployeHeader />
+            <h1 className='text-center'>Modifier un livre</h1>
+            <div className='p-5'>
+                <DocumentCard document={livre} />
+            </div>
+            <form className='container pb-4' onSubmit={onSubmit} >
+                <div>
+                    <label className='form-label' >Titre</label>
+                    <input className='form-control ' type="text" placeholder="3-200 characters"
+                        required minLength={3} maxLength={200}
+                        onChange={(e) => { setTitre(e.target.value) }} />
+                </div>
+                <div>
+                    <label className='form-label'>Auteur</label>
+                    <input className='form-control ' type="text" placeholder="3-200 characters"
+                        required minLength={3} maxLength={200}
+                        onChange={(e) => { setAuteur(e.target.value) }} />
+                </div>
+                <div>
+                    <label className='form-label' >Editeur</label>
+                    <input className='form-control ' type="text" placeholder="3-200 characters"
+                        required minLength={3} maxLength={200}
+                        onChange={(e) => { setEditeur(e.target.value) }} />
+                </div>
+                <div>
+                    <label className='form-label' >Année de publication</label>
+                    <input className='form-control ' type="number"
+                        required maxLength={4}
+                        onChange={(e) => { setDate(e.target.value.toString()) }} />
+                </div>
+                <div>
+                    <label className='form-label' >Nombre d'exemplaires</label>
+                    <input className='form-control ' type="number"
+                        required minLength={1} maxLength={10} min={1}
+                        onChange={(e) => { setNbEx(e.target.value.toString()) }} />
+                </div>
+                <div>
+                    <label className='form-label' >Nombre de pages</label>
+                    <input className='form-control ' type="number"
+                        required minLength={1} maxLength={7} min={1}
+                        onChange={(e) => { setNbPages(e.target.value.toString()) }} />
+                </div>
+                <div>
+                    <label className='form-label' >Genre</label>
+                    <select className='form-select'
+                        onChange={(e) => { setGenre(e.target.value) }} required>
+                        <option value="" selected disabled>Choisir une valeur</option>
+                        <option value={"roman"}>Roman</option>
+                        <option value={"manuel"}>Manuel</option>
+                        <option value={"magazine"}>Magazine</option>
+                        <option value={"etude"}>Etude</option>
+                    </select >
+                </div>
+                <div className='row d-flex justify-content-center'>
+                    <button className='btn btn-primary mt-4 col-4'>Envoyer</button>
+                </div>
+            </form>
+        </>
+    )
+}
+
---
+++ tp4react/src/Components/PageServices/EmployeeService/Update/updateMedia/mediaUpdateForm.js
@@ -0,0 +1,88 @@
+import React from 'react'
+import { useState } from 'react'
+import { EmployeHeader } from '../../../../Headers/employeHeader';
+import { DocumentCard } from '../../../UserService/Documents/documentCard';
+
+export const MediaUpdateForm = ({ submit, livre: media }) => {
+
+    const [titre, setTitre] = useState('');
+    const [auteur, setAuteur] = useState('');
+    const [editeur, setEditeur] = useState('');
+    const [anneeDePublication, setDate] = useState("2000");
+    const [nbExemplaires, setNbEx] = useState("1");
+    const [duree, setDuree] = useState("1");
+    const [type, setType] = useState("dvd");
+
+    const onSubmit = (e) => {
+        let tempsEmprunt = type == "cd" ? "2" : "1";
+        const mediaUpdated = { titre, auteur, editeur, anneeDePublication, tempsEmprunt, nbExemplaires, duree, type }
+        e.preventDefault()
+        submit(mediaUpdated,media.documentId)
+    }
+    if(media.titre == undefined){
+        return <>
+            <EmployeHeader />
+            <h1 className='text-center text-danger'>Pas de média</h1>
+        </>
+    }
+    return (
+        <>
+            <EmployeHeader />
+            <h1 className='text-center'>Modifier un média</h1>
+            <div className='p-5'>
+                <DocumentCard document={media} />
+            </div>
+            <form className='container pb-5' onSubmit={onSubmit} >
+            <div>
+                <label className='form-label' >Titre</label>
+                <input className='form-control ' type="text" placeholder="3-200 characters"
+                    required minLength={3} maxLength={200}
+                    onChange={(e) => { setTitre(e.target.value) }} />
+            </div>
+            <div>
+                <label className='form-label'>Auteur</label>
+                <input className='form-control ' type="text" placeholder="3-200 characters"
+                    required minLength={3} maxLength={200}
+                    onChange={(e) => { setAuteur(e.target.value) }} />
+            </div>
+            <div>
+                <label className='form-label' >Editeur</label>
+                <input className='form-control ' type="text" placeholder="3-200 characters"
+                    required minLength={3} maxLength={200}
+                    onChange={(e) => { setEditeur(e.target.value) }} />
+            </div>
+            <div>
+                <label className='form-label' >Année de publication</label>
+                <input className='form-control ' type="number"
+                    required maxLength={4}
+                    onChange={(e) => { setDate(e.target.value.toString()) }} />
+            </div>
+            <div>
+                <label className='form-label' >Nombre d'exemplaires</label>
+                <input className='form-control ' type="number"
+                    required minLength={1} maxLength={10} min={1}
+                    onChange={(e) => { setNbEx(e.target.value.toString()) }} />
+            </div>
+            <div>
+                <label className='form-label' >Durée</label>
+                <input className='form-control ' type="Text" placeholder="Format: 0-99999h59min"
+                    required minLength={7} maxLength={11} min={1} pattern="[0-9]*h[0-5][0-9]min"
+                    onChange={(e) => { setDuree(e.target.value) }} />
+            </div>
+            <div>
+                <label className='form-label' >Type</label>
+                <select className='form-select'
+                    onChange={(e) => { setType(e.target.value) }} required>
+                    <option value="" selected disabled>Choisir une valeur</option>
+                    <option value={"cd"}>CD</option>
+                    <option value={"dvd"}>DVD</option>
+                </select >
+            </div>
+            <div className='row d-flex justify-content-center'>
+                <button className='btn btn-primary mt-4 col-4'>Envoyer</button>
+            </div>
+        </form>
+        </>
+    )
+}
+
